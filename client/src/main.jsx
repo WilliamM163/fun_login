86a9768ab6d21623239c873c80da61226841e5d6
William Massey <william.massey.163@gmail.com>
2025-03-16 15:52:26 +1300

    Removed Testing Route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./routes/home.jsx";
 import Register from "./routes/register.jsx";
 import Login from "./routes/login.jsx";
-import TestingApi from "./routes/testing_api.jsx";
 import Error from "./routes/error.jsx";
 
 createRoot(document.getElementById('root')).render(
@@ -15,7 +14,6 @@ createRoot(document.getElementById('root')).render(
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/testing_api" element={<TestingApi />} />
                 <Route path="/error" element={<Error />} />
             </Routes>
         </BrowserRouter>
